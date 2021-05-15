6f00ccb48ea23195bae9aa292d294eaaf6f4b65d
sonuhalder24 <haldersonu36@gmail.com>
2021-05-15 15:37:14 +0530

    Java and gradle file modified

--- app/src/main/java/com/example/youtubedownloader/MainActivity.java
+++ app/src/main/java/com/example/youtubedownloader/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.youtubedownloader;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -21,21 +37,23 @@ public class MainActivity extends AppCompatActivity {
     int index;
     long delay=200;
     Handler handler=new Handler();
-    
+    String pattern = "https?:\\/\\/(?:[0-9A-Z-]+\\.)?(?:youtu\\.be\\/|youtube\\.com\\S*[^\\w\\-\\s])([\\w\\-]{11})(?=[^\\w\\-]|$)(?![?=&+%\\w]*(?:['\"][^<>]*>|<\\/a>))[?=&+%\\w]*";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        
+
         relativeLayout=findViewById(R.id.relativeLay);
         textView=findViewById(R.id.text_view);
         headphone=findViewById(R.id.headphone);
         editText=findViewById(R.id.editText);
         mp4=findViewById(R.id.mp4);
         mp3=findViewById(R.id.mp3);
-        
+
+        serverInitialize();
+
         ObjectAnimator objectAnimator=ObjectAnimator.ofPropertyValuesHolder(
                 headphone,
                 PropertyValuesHolder.ofFloat("scaleX",1.2f),
@@ -59,27 +77,43 @@ public class MainActivity extends AppCompatActivity {
                 getSupportActionBar().show();
             }
         });
-
+        //go MP4 Activity
         mp4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String link=editText.getText().toString().trim();
-                Intent intent=new Intent(MainActivity.this,WebActivity.class);
-                intent.putExtra("linkvideo",link);
-                startActivity(intent);
+                if(!editText.getText().toString().trim().isEmpty() &&
+                        editText.getText().toString().trim().matches(pattern))
+                {
+                    String link = editText.getText().toString().trim();
+                    Intent intent = new Intent(MainActivity.this, WebActivity.class);
+                    intent.putExtra("linkvideo", link);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please provide a valid url", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+        //go MP3 Activity
         mp3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String link=editText.getText().toString().trim();
-                Intent intent=new Intent(MainActivity.this,WebActivity2.class);
-                intent.putExtra("linkaudio",link);
-                startActivity(intent);
+                if(!editText.getText().toString().trim().isEmpty() &&
+                        editText.getText().toString().trim().matches(pattern))
+                {
+                    String link = editText.getText().toString().trim();
+                    Intent intent = new Intent(MainActivity.this, WebActivity2.class);
+                    intent.putExtra("linkaudio", link);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Please provide a valid url", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
-    
+
+
     Runnable runnable=new Runnable() {
         @Override
         public void run() {
@@ -115,4 +149,22 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void serverInitialize() {
+        RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+        String url="ENTER YOUR HEROKU SERVER LINK";
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
 }
--- app/src/main/java/com/example/youtubedownloader/WebActivity2.java
+++ app/src/main/java/com/example/youtubedownloader/WebActivity2.java
@@ -17,133 +17,87 @@ import android.webkit.URLUtil;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.jetbrains.annotations.NotNull;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.airbnb.lottie.LottieAnimationView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class WebActivity2 extends AppCompatActivity {
     TextView empty_text_mp3;
-    WebView webView2;
-    String json;
+    WebView webView;
+    LottieAnimationView loadingAnim2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web2);
-        webView2=findViewById(R.id.webView2);
+        webView=findViewById(R.id.webView2);
         empty_text_mp3=findViewById(R.id.empty_text2);
+        loadingAnim2=findViewById(R.id.loading2);
         String link=getIntent().getStringExtra("linkaudio");
 
-        OkHttpClient client = new OkHttpClient();
+        //API call using Volley
 
-        Request request = new Request.Builder()
-                .url("https://immotile-diaries.000webhostapp.com?url="+link)
-                .get()
-                .build();
-        client.newCall(request).enqueue(new Callback() {
+        loadingAnim2.setVisibility(View.VISIBLE);
+        RequestQueue requestQueue= Volley.newRequestQueue(WebActivity2.this);
+        String url="ENTER YOUR HEROKU SERVER LINK"+link;
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new com.android.volley.Response.Listener<String>() {
             @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                empty_text_mp3.setText(R.string.no_internet);
-                e.printStackTrace();
+            public void onResponse(String response) {
+                webView.setWebViewClient(new WebViewClient());
+                loadingAnim2.setVisibility(View.GONE);
+                webView.loadUrl(response);
+
             }
-
+        }, new Response.ErrorListener() {
             @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                if(response.isSuccessful()){
-                    final String myResponse=response.body().string();
-                    WebActivity2.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            boolean status=false;
-                            try{
-                                JSONObject baseJsonResponse=new JSONObject(myResponse);
-                                status=baseJsonResponse.getBoolean("status");
-                                if(status==true) {
-                                    JSONArray streamsArray = baseJsonResponse.getJSONArray("streams");
-                                    JSONObject firstFeature = streamsArray.getJSONObject(1);
-                                    json = firstFeature.getString("url");
-                                }
-                                else{
-                                    Toast.makeText(WebActivity2.this, "Please provide a valid url", Toast.LENGTH_SHORT).show();
-                                }
+            public void onErrorResponse(VolleyError error) {
+                loadingAnim2.setVisibility(View.GONE);
+                empty_text_mp3.setText(R.string.problem);
 
-                            }catch(Exception e){
-                                e.printStackTrace();
-                            }
+            }
+        });
+        requestQueue.add(stringRequest);
 
-                            webView2.getSettings().setLoadsImagesAutomatically(true);
-                            webView2.getSettings().setJavaScriptEnabled(true);
-                            webView2.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-                            webView2.setWebViewClient(new WebViewClient());
-                            webView2.loadUrl(json);
+        //External storage permission for saving file
 
-                            //added
-                            WebSettings mywebsettings = webView2.getSettings();
-                            mywebsettings.setJavaScriptEnabled(true);
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
 
-                            webView2.setWebViewClient(new WebViewClient());
+                Log.d("permission","permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions,1);
+            }
+        }
 
+        //handle downloading
 
-                            webView2.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
-                            webView2.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-                            webView2.getSettings().setAppCacheEnabled(true);
-                            webView2.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-                            mywebsettings.setDomStorageEnabled(true);
-                            mywebsettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-                            mywebsettings.setUseWideViewPort(true);
-                            mywebsettings.setSavePassword(true);
-                            mywebsettings.setSaveFormData(true);
-                            mywebsettings.setEnableSmoothTransition(true);
-                            //External storage permission for saving file
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+                public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
 
-                            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
-                                if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                    request.setMimeType("audio/mpeg");
+                    String cookies = CookieManager.getInstance().getCookie(url);
+                    request.addRequestHeader("cookie",cookies);
+                    request.addRequestHeader("User-Agent",userAgent);
+                    request.setDescription("Downloading file....");
+                    request.setTitle(URLUtil.guessFileName(url,contentDisposition,"audio/mpeg"));
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,URLUtil.guessFileName(url, contentDisposition, "audio/mpeg"));
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    dm.enqueue(request);
+                    Toast.makeText(getApplicationContext(),"Downloading File",Toast.LENGTH_SHORT).show();
 
-                                    Log.d("permission","permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
-                                    String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                                    requestPermissions(permissions,1);
-                                }
-
-
-                            }
-
-                            //handle downloading
-
-                            webView2.setDownloadListener(new DownloadListener() {
-                                @Override
-                                public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
-
-                                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                                    request.setMimeType("audio/mpeg");
-                                    String cookies = CookieManager.getInstance().getCookie(url);
-                                    request.addRequestHeader("cookie",cookies);
-                                    request.addRequestHeader("User-Agent",userAgent);
-                                    request.setDescription("Downloading file....");
-                                    request.setTitle(URLUtil.guessFileName(url,contentDisposition,"audio/mpeg"));
-                                    request.allowScanningByMediaScanner();
-                                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,URLUtil.guessFileName(url, contentDisposition, "audio/mpeg"));
-                                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                                    dm.enqueue(request);
-                                    Toast.makeText(getApplicationContext(),"Downloading File",Toast.LENGTH_SHORT).show();
-
-
-                                }
-                            });
-                        }
-                    });
-                }
             }
         });
     }
+
 }
